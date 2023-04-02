3937163e39243674b384f7f7ae6805e9cce25ae1
Pedro Mantovani <mantovani.pedro99@gmail.com>
2023-04-02 18:10:26 -0300

    innitial

---
+++ server/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB Connected!");
+  })
+  .catch((err) => console.error(err));
+
+app.use("/api/post", require("./routers/routes"));
+
+app.listen(port, () => console.log("Server running!"));
